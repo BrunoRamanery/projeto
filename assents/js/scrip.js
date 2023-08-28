2d2366ffef80d233074c45d0d57fc482713db951
BrunoRamanery <ramanery00@gmail.com>
2023-08-28 16:54:16 -0300

    new

--- assents/js/scrip.js
+++ assents/js/scrip.js
@@ -1,17 +1,18 @@
-const mode = document.getElementById('mode_icon');
+
+const mode = document.getElementById('mode_icon');  /*marca o icone lua*/
 
 mode.addEventListener('click', () => {
-    const form = document.getElementById('login_form');
+    const form = document.getElementById('cadastro_form');  /*o if troca o simbolo de lua pelo simplo de sol*/
 
     if(mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
 
-        form.classList.add('dark');
+        form.classList.add('dark');       /*adiciona a classe escuro*/
         return ;
     }
 
-    mode.classList.remove('fa-sun');
+    mode.classList.remove('fa-sun');   /*troca o sol pela lua*/
     mode.classList.add('fa-moon');
 
     form.classList.remove('dark');
